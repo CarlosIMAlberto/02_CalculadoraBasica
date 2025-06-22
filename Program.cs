dd1f4350bdc063572070210ccccd2e5b5d91a8bc
CarlosIMAlberto <carlosalberto.amic@gmail.com>
2025-06-22 22:12:54 +0100

    Adiciona projeto da Calculadora Básica em C#
    Inclui funcionalidades de adição e subtração via entrada do usuário no console.
    Projeto criado com objetivo de praticar lógica de programação, manipulação de variáveis e operações aritméticas simples.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variaveis
+            float a, b;
+            char sinal;
+            float resultado;
+            bool controlador;
+            #endregion
+
+            #region Input
+            Console.WriteLine("Digite um número");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um segundo número");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a sinal aritimétrico + ou - ");
+            sinal = char.Parse(Console.ReadLine());
+            #endregion  
+            if (sinal =='+' || sinal == '-')
+            {
+                if (sinal == '+')
+                {
+                    resultado = a + b;
+                    Console.WriteLine(resultado);
+
+                }
+
+                else if (sinal == '-')
+                {
+                    resultado = a - b;
+                    Console.WriteLine(resultado);
+                }
+                
+            }
+            else
+            {
+
+                controlador = true;
+                while (controlador)
+                {
+                    Console.WriteLine("Digite apenas ou + ou menos sem epaço");
+                    sinal = char.Parse(Console.ReadLine());
+                    if (sinal == '+' || sinal == '-')
+                    {
+                        break;
+                    }
+                }
+                if (sinal == '+')
+                {
+                    resultado = a + b;
+                    Console.WriteLine(resultado);
+
+                }
+
+                else if (sinal == '-')
+                {
+                    resultado = a - b;
+                    Console.WriteLine(resultado);
+                }
+            }
+            //Console.ReadKey();
+            Console.ReadLine();
+        }
+
+    }
+}
